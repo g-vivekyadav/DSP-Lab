c063f1cb54128b05fa76c775766761f2006d344e
g-vivekyadav <89974573+g-vivekyadav@users.noreply.github.com>
2022-01-12 14:20:43 +0530

    Update DFS_and_BFS.c

--- week 9/DFS_and_BFS.c
+++ week 9/DFS_and_BFS.c
@@ -4,9 +4,9 @@ int n,f=0,r=-1,m=-1,*queue,*visited,*values;
 void DFS(int i,int* graph[n])
 {
     int j;
-	printf("%d ",i);
+    printf("%d ",i);
     visited[i]=1;
-	for(j=0;j<n;j++)
+    for(j=0;j<n;j++)
         if(!visited[j] && graph[i][j]==1)
             DFS(j,graph);
 }
@@ -35,24 +35,25 @@ void main()
     int i,j,choice=0;
     while(choice !=2){
     	printf("\nEnter the choice 1.Number of vertices 2.Exit\n");
-		scanf("%d",&choice);
-		if(choice==1){
-			printf("Enter the no. of vertices\n");
-			scanf("%d",&n);
-			int* graph[n];
+	scanf("%d",&choice);
+	if(choice==1){
+		printf("Enter the no. of vertices\n");
+		scanf("%d",&n);
+		int* graph[n];
     		for (i = 0; i < n; i++)
         		graph[i] = (int*)malloc(n * sizeof(int));
-				visited = (int*)malloc(n* sizeof(int));
-				values = (int*)malloc(n* sizeof(int));
+			visited = (int*)malloc(n* sizeof(int));
+			values = (int*)malloc(n* sizeof(int));
     	//read the adjecency matrix
 			printf("\nEnter adjacency matrix of the graph:\n");
 			for(i=0;i<n;i++)
      			for(j=0;j<n;j++)
 					scanf("%d",&graph[i][j]);
     	//visited is initialized to zero
-   			for(i=0;i<n;i++)
-    		    visited[i]=0;
-    		    values[i]=-1;
+   			for(i=0;i<n;i++){
+    		    		visited[i]=0;
+    		    		values[i]=-1;
+			}
     		printf("\nEnter the choice 1. DFS 2.BFS\n");
     		int choice2;
     		scanf("%d",&choice2);
@@ -63,8 +64,7 @@ void main()
     			for(i=0;i<n;i++)
     				queue[i]=0;
     			BFS(0,graph);
-    			printf("0 ");
-    			for(i=0;i<n-1;i++){
+    			for(i=0;i<n;i++){
     				printf("%d ",values[i]);
 				}
     		}
