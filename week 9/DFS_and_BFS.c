fcf744f4a7fe73319fa4e22af0db9387e78a8b1d
g-vivekyadav <89974573+g-vivekyadav@users.noreply.github.com>
2022-01-12 15:53:53 +0530

    Update DFS_and_BFS.c

--- week 9/DFS_and_BFS.c
+++ week 9/DFS_and_BFS.c
@@ -4,9 +4,9 @@ int n,f=0,r=-1,m=-1,*queue,*visited,*values;
 void DFS(int i,int* graph[n])
 {
     int j;
-    printf("%d ",i);
+	values[++m]=i;
     visited[i]=1;
-    for(j=0;j<n;j++)
+	for(j=0;j<n;j++)
         if(!visited[j] && graph[i][j]==1)
             DFS(j,graph);
 }
@@ -35,39 +35,83 @@ void main()
     int i,j,choice=0;
     while(choice !=2){
     	printf("\nEnter the choice 1.Number of vertices 2.Exit\n");
-	scanf("%d",&choice);
-	if(choice==1){
-		printf("Enter the no. of vertices\n");
-		scanf("%d",&n);
-		int* graph[n];
+		if(scanf("%d",&choice) != 1){
+			printf("invalid choice. enter the number\n");
+			exit(0);
+		}
+		if(choice==1){
+			printf("Enter the no. of vertices\n");
+			if(scanf("%d",&n) != 1){
+				printf("invalid choice. enter the number\n");
+				exit(0);
+			}
+			int* graph[n];
     		for (i = 0; i < n; i++)
         		graph[i] = (int*)malloc(n * sizeof(int));
-			visited = (int*)malloc(n* sizeof(int));
-			values = (int*)malloc(n* sizeof(int));
+				visited = (int*)malloc(n* sizeof(int));
+				values = (int*)malloc(n* sizeof(int));
     	//read the adjecency matrix
 			printf("\nEnter adjacency matrix of the graph:\n");
-			for(i=0;i<n;i++)
-     			for(j=0;j<n;j++)
-					scanf("%d",&graph[i][j]);
+			for(i=0;i<n;i++){
+     			for(j=0;j<n;j++){
+					if(scanf("%d",&graph[i][j]) !=1){
+						printf("Invalid data given. Enter only numbers");
+						exit(0);
+					}
+				}
+			}
     	//visited is initialized to zero
    			for(i=0;i<n;i++){
-    		    		visited[i]=0;
-    		    		values[i]=-1;
-			}
+    		    visited[i]=0;
+    		    values[i]=-1;
+    		}
     		printf("\nEnter the choice 1. DFS 2.BFS\n");
     		int choice2;
     		scanf("%d",&choice2);
-    		if(choice2==1)
-    		DFS(0,graph);
+    		int checking=0,startnode;
+    		if(choice2==1){
+    			printf("Enter the start node\n");
+    			scanf("%d",&startnode);
+    			if(startnode >-1 && startnode < n)
+    				DFS(startnode,graph);
+    			else
+    				printf("Invalid start node. try again\n");
+    		for(i=0;i<n;i++){
+    				if(values[i] == -1);
+    					checking=1;
+    					break;
+				}
+				if(checking==1)
+					printf("Not reachable to all nodes\n\n");
+				else{
+					for(i=0;i<n;i++){
+    				printf("%d ",values[i]);
+				}	
+				}
+    		}
     		else if(choice2==2){
     			queue=(int*)malloc(sizeof(int)*n);
     			for(i=0;i<n;i++)
     				queue[i]=0;
-    			BFS(0,graph);
+    			printf("Enter the start node\n");
+    			scanf("%d",&startnode);
+    			if(startnode >-1 && startnode < n)
+    				BFS(startnode,graph);
+    			else
+    				printf("Invalid start node. try again\n");	
     			for(i=0;i<n;i++){
-    				printf("%d ",values[i]);
+    				if(values[i] == -1);
+    					checking=1;
+    					break;
 				}
-    		}
+				if(checking==1)
+					printf("Not reachable to all nodes\n\n");
+				else{
+					for(i=0;i<n;i++){
+    				printf("%d ",values[i]);
+				}	
+				}
+				}
     		else
     			printf("\nInvalid choice\n");
 		}
